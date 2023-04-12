76b43e341f4042ec7102ed67e752312d20d9f644
Zackaria Antar <antarzack@gmail.com>
2023-04-12 17:18:51 -0500

    Answered 4 - 6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -16,10 +16,12 @@ console.log('3. Second supplyChange is:', supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+console.log('4. Removed item:', supplyChanges.pop());
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
+supplyChanges.push(25)
 console.log('5. Adding 25 to supplyChanges.');
+
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -27,7 +29,15 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for (x of supplyChanges){
+    if (x > 0){
+        console.log(`Added ${x} parts.`);
+    }else if (x===0){
+        console.log('No change.');
+    }else if (x < 0){
+        console.log(`Removed ${x} parts.`);
+    }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
